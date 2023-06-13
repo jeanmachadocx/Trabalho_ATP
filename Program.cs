d9e99c021e59bc75930ea9254c111cb7030a011e
Jean Machado <79550947+jeanmachadocx@users.noreply.github.com>
2023-06-13 11:29:24 -0300

    Alterações feitas no laboratório dia 13/6

--- Program.cs
+++ Program.cs
@@ -89,26 +89,39 @@
     //METODO QUE REVELA AS POSIÇÕES ADJCENTES 
     public static char[,] revelamapa(char[,] m1, char[,] m2, int a, int b)
     {
-        m1[a, b] = m2[a, b];
+        int linhas = m1.GetLength(0);
+        int colunas = m1.GetLength(1);
 
-        // PRINTA A POSIÇÃO ACIMA E ABAIXO 
-        m1[a - 1, b] = m2[a - 1, b];
-        m1[a + 1, b] = m2[a + 1, b];
+        if (a >= 0 && a < linhas && b >= 0 && b < colunas)
+        {
+            m1[a, b] = m2[a, b];
 
-        // PRINTA ESQUERDA E DIREITA 
-        m1[a, b - 1] = m2[a, b - 1];
-        m1[a, b + 1] = m2[a, b + 1];
+            // Verificações para as posições acima e abaixo
+            if (a - 1 >= 0)
+                m1[a - 1, b] = m2[a - 1, b];
+            if (a + 1 < linhas)
+                m1[a + 1, b] = m2[a + 1, b];
 
-        // PRINTA AS DIAGONAIS SUPERIORES
-        m1[a - 1, b - 1] = m2[a - 1, b - 1];
-        m1[a - 1, b + 1] = m2[a - 1, b + 1];
+            // Verificações para as posições à esquerda e à direita
+            if (b - 1 >= 0)
+                m1[a, b - 1] = m2[a, b - 1];
+            if (b + 1 < colunas)
+                m1[a, b + 1] = m2[a, b + 1];
 
-        // PRINTA AS DIAGONAIS INFERIORES
-        m1[a + 1, b - 1] = m2[a + 1, b - 1];
-        m1[a + 1, b + 1] = m2[a + 1, b + 1];
+            // Verificações para as diagonais superiores
+            if (a - 1 >= 0 && b - 1 >= 0)
+                m1[a - 1, b - 1] = m2[a - 1, b - 1];
+            if (a - 1 >= 0 && b + 1 < colunas)
+                m1[a - 1, b + 1] = m2[a - 1, b + 1];
+
+            // Verificações para as diagonais inferiores
+            if (a + 1 < linhas && b - 1 >= 0)
+                m1[a + 1, b - 1] = m2[a + 1, b - 1];
+            if (a + 1 < linhas && b + 1 < colunas)
+                m1[a + 1, b + 1] = m2[a + 1, b + 1];
+        }
 
         return m1;
-        //char[,] campo = revelamapa(mapaX, mapa, linhap, colunap);
     }
 
     // METODO QUE PRINTA UM MAPA COBERTO
@@ -143,7 +156,7 @@
         // VARIAVEL QUE RECEBERA DO USUÁRIO O LOCAL ONDE ESTA O MAPA 
 
         // Console.WriteLine("Digite o caminho do arquivo mapa: ");
-        string caminho = @"C:\mapasteste\mapa1.txt";
+        string caminho = @"C:\mapasteste\mapa.txt";
 
 
 
@@ -188,7 +201,25 @@
                 if (verifica == true)
                 {
                     wl = false;
+                    Console.Write("    A B C D E F G H I\n");
+                    int cont = 1;
+                    for (int g = 0; g < linhas; g++)
+                    {
+                        if (cont > 9)
+                        {
+                            Console.Write(cont++ + "- ");
+                        }
+                        else
+                            Console.Write(cont++ + " - ");
+                        for (int a = 0; a < colunas; a++)
+                        {
+                            Console.Write(campo[g, a] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
                     Console.Write("VOCÊ PERDEU!");
+
                 }
                 else
                 {
@@ -212,7 +243,6 @@
                 }
 
             }
-
         }
         catch (FileNotFoundException) // VERIFICA SE O CAMINHO INFORMADO EXISTE 
         {
