784efff1523fccc1c7ec60a682be5b73a9ef5fa2
guilherme_silveira <Guilhermehs6465@gmail.com>
2023-06-21 22:31:45 -0300

    FINALIZADO

--- Metodos.cs
+++ Metodos.cs
@@ -1,7 +1,7 @@
 class Metodos
 {
     // METODO QUE VERIFICA DERROTA
-    public static bool vrfderrota(char[,] matriz, int a, int b)
+    public static bool VerificaDerrota(char[,] matriz, int a, int b)
 
 
     {
@@ -15,45 +15,8 @@ class Metodos
         return derrota;
     }
 
-    // METODO VERIFICA VITORIA
-    public static bool VerfWin(char[,] matriz, char[,] gabarito, int a, int b)
-    {
-        bool vitoria = false;
-        int contaX = 0;
-        int contaB = 0;
-
-        for (int i = 0; i < a; i++)
-        {
-            for (int j = 0; j < b; j++)
-            {
-                if (matriz[i, j] == 'X')
-                {
-                    contaX++;
-                }
-            }
-        }
-        for (int i = 0; i < a; i++)
-        {
-            for (int j = 0; j < b; j++)
-            {
-                if (gabarito[i, j] == 'B')
-                {
-                    contaB++;
-                }
-            }
-        }
-        if (contaX != contaB)
-        {
-            vitoria = false;
-        }
-        else
-            vitoria = true;
-
-        return vitoria;
-    }
-
     // METODO QUE CRIA UM MAPA COBERTO
-    public static char[,] mapacoberto(int a, int b)
+    public static char[,] MapaCoberto(int a, int b)
     {
 
         char[,] mapaX = new char[a, b];
@@ -68,119 +31,6 @@ class Metodos
 
         return mapaX;
 
-    }
-
-    //METODO QUE REVELA AS POSIÇÕES ADJCENTES 
-    public static char[,] RevelaMapa(char[,] m1, char[,] m2, int a, int b, int l, int c)
-    {
-
-
-        if (a >= 0 && a < l && b >= 0 && b < c)
-        {
-            m1[a, b] = m2[a, b]; // m2 = mapa com X e m1 = gabarito 
-
-            // Verificações para as posições acima e abaixo
-            if (a - 1 >= 0)
-            {
-                m1[a - 1, b] = m2[a - 1, b];
-                if (m2[a - 1, b] == 'B')
-                {
-                    m1[a - 1, b] = 'X';
-                }
-            }
-            if (a + 1 < l)
-            {
-                m1[a + 1, b] = m2[a + 1, b];
-                if (m2[a + 1, b] == 'B')
-                {
-                    m1[a + 1, b] = 'X';
-                }
-            }
-            // Verificações para as posições à esquerda e à direita
-
-            if (b - 1 >= 0) //esquerda
-            {
-                m1[a, b - 1] = m2[a, b - 1];
-                if (m2[a, b - 1] == 'B')
-                {
-                    m1[a, b - 1] = 'X';
-                }
-            }
-            if (b + 1 < c) //DIREITA 
-            {
-                m1[a, b + 1] = m2[a, b + 1];
-                if (m2[a, b + 1] == 'B')
-                {
-                    m1[a, b + 1] = 'X';
-                }
-            }
-            // Verificações para as diagonais superiores
-            if (a - 1 >= 0 && b - 1 >= 0)
-            {
-                m1[a - 1, b - 1] = m2[a - 1, b - 1];
-                if (m2[a - 1, b - 1] == 'B')
-                {
-                    m1[a - 1, b - 1] = 'X';
-                }
-            }
-            if (a - 1 >= 0 && b + 1 < c)
-            {
-                m1[a - 1, b + 1] = m2[a - 1, b + 1];
-                if (m2[a - 1, b + 1] == 'B')
-                {
-                    m1[a - 1, b + 1] = 'X';
-                }
-            }
-            // Verificações para as diagonais inferiores
-            if (a + 1 < l && b - 1 >= 0)
-            {
-                m1[a + 1, b - 1] = m2[a + 1, b - 1];
-                if (m2[a + 1, b - 1] == 'B')
-                {
-                    m1[a + 1, b - 1] = 'X';
-                }
-            }
-            if (a + 1 < l && b + 1 < c)
-            {
-                m1[a + 1, b + 1] = m2[a + 1, b + 1];
-                if (m2[a + 1, b + 1] == 'B')
-                {
-                    m1[a + 1, b + 1] = 'X';
-                }
-            }
-        }
-
-        return m1;
-    }
-
-    // METODO QUE PRINTA UM MAPA COBERTO
-    public static void printamapaCoberto(int a, int b)
-    {
-        int cont = 1;
-        char[,] m = new char[a, b];
-        int[] indicaColunas = new int[b];
-        for (int i = 0; i < a; i++)
-        {
-            if (cont > 9)
-            {
-                Console.Write(cont++ + "- ");
-            }
-            else
-                Console.Write(cont++ + " - ");
-            for (int j = 0; j < b; j++)
-            {
-                m[i, j] = 'X';
-                Console.Write(m[i, j] + " ");
-            }
-            Console.WriteLine();
-        }
-        Console.Write("\n" + "    ");
-        for (int i = 0; i < b; i++)
-        {
-            indicaColunas[i] = i + 1;
-            Console.Write(indicaColunas[i] + " ");
-        }
-        Console.WriteLine("\n");
     }
 
     //METODO QUE PREENCHE A MATRIZ
@@ -281,4 +131,155 @@ class Metodos
         }
         return mapa;
     }
+
+// METODO VERIFICA VITORIA
+public static bool VerificafWin(char[,] matriz, char[,] gabarito, int a, int b)
+{
+    bool vitoria = false;
+    int contaX = 0;
+    int contaB = 0;
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (matriz[i, j] == 'X')
+            {
+                contaX++;
+            }
+        }
+    }
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if (gabarito[i, j] == 'B')
+            {
+                contaB++;
+            }
+        }
+    }
+    if (contaX != contaB)
+    {
+        vitoria = false;
+    }
+    else
+        vitoria = true;
+
+    return vitoria;
+}
+
+//METODO QUE REVELA AS POSIÇÕES ADJCENTES 
+public static char[,] RevelaMapa(char[,] m1, char[,] m2, int a, int b, int l, int c)
+{
+
+
+    if (a >= 0 && a < l && b >= 0 && b < c)
+    {
+        m1[a, b] = m2[a, b]; // m2 = mapa com X e m1 = gabarito 
+
+        // Verificações para as posições acima e abaixo
+        if (a - 1 >= 0)
+        {
+            m1[a - 1, b] = m2[a - 1, b];
+            if (m2[a - 1, b] == 'B')
+            {
+                m1[a - 1, b] = 'X';
+            }
+        }
+        if (a + 1 < l)
+        {
+            m1[a + 1, b] = m2[a + 1, b];
+            if (m2[a + 1, b] == 'B')
+            {
+                m1[a + 1, b] = 'X';
+            }
+        }
+        // Verificações para as posições à esquerda e à direita
+
+        if (b - 1 >= 0) //esquerda
+        {
+            m1[a, b - 1] = m2[a, b - 1];
+            if (m2[a, b - 1] == 'B')
+            {
+                m1[a, b - 1] = 'X';
+            }
+        }
+        if (b + 1 < c) //DIREITA 
+        {
+            m1[a, b + 1] = m2[a, b + 1];
+            if (m2[a, b + 1] == 'B')
+            {
+                m1[a, b + 1] = 'X';
+            }
+        }
+        // Verificações para as diagonais superiores
+        if (a - 1 >= 0 && b - 1 >= 0)
+        {
+            m1[a - 1, b - 1] = m2[a - 1, b - 1];
+            if (m2[a - 1, b - 1] == 'B')
+            {
+                m1[a - 1, b - 1] = 'X';
+            }
+        }
+        if (a - 1 >= 0 && b + 1 < c)
+        {
+            m1[a - 1, b + 1] = m2[a - 1, b + 1];
+            if (m2[a - 1, b + 1] == 'B')
+            {
+                m1[a - 1, b + 1] = 'X';
+            }
+        }
+        // Verificações para as diagonais inferiores
+        if (a + 1 < l && b - 1 >= 0)
+        {
+            m1[a + 1, b - 1] = m2[a + 1, b - 1];
+            if (m2[a + 1, b - 1] == 'B')
+            {
+                m1[a + 1, b - 1] = 'X';
+            }
+        }
+        if (a + 1 < l && b + 1 < c)
+        {
+            m1[a + 1, b + 1] = m2[a + 1, b + 1];
+            if (m2[a + 1, b + 1] == 'B')
+            {
+                m1[a + 1, b + 1] = 'X';
+            }
+        }
+    }
+
+    return m1;
+}
+
+// METODO QUE PRINTA UM MAPA COBERTO
+public static void PrintaMapaCoberto(int a, int b)
+{
+    int cont = 1;
+    char[,] m = new char[a, b];
+    int[] indicaColunas = new int[b];
+    for (int i = 0; i < a; i++)
+    {
+        if (cont > 9)
+        {
+            Console.Write(cont++ + "- ");
+        }
+        else
+            Console.Write(cont++ + " - ");
+        for (int j = 0; j < b; j++)
+        {
+            m[i, j] = 'X';
+            Console.Write(m[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.Write("\n" + "    ");
+    for (int i = 0; i < b; i++)
+    {
+        indicaColunas[i] = i + 1;
+        Console.Write(indicaColunas[i] + " ");
+    }
+    Console.WriteLine("\n");
+}
+
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
-﻿
+﻿// O trabalho prático consiste na implementação de um jogo, Campo Minado. O objetivo trata-se  de ler um arquivo de texto onde encontram-se as 
+// informações sobre a quantidade de linhas, colunas e bombas. Após isso, construir um mapa correspondente às informações e fazer com que o usuário 
+// consiga jogá-lo ao inserir a linha e coluna desejada . 
+
+//Guilherme Henrique, Jean Carlo, Vinícius Morais, Lucca Cenisio 
 class Program
 {
     static void Main()
     {
 
         // VARIAVEL QUE RECEBERA DO USUÁRIO O LOCAL ONDE ESTA O MAPA 
-
-        string caminho = @"C:\mapasteste\mapa1.txt";
+        Console.WriteLine("Digite o caminho do arquivo:");
+        string caminho = Console.ReadLine();
 
         int linhas = 0, colunas = 0, bombas = 0;
         try
@@ -22,7 +26,7 @@ class Program
 
                 for (int i = 0; i < partes.Length - 2; i++)
                 {
-                     // Tenta converter as partes em inteiros 
+                    // Tenta converter as partes em inteiros 
                     if (int.TryParse(partes[i], out linhas) && int.TryParse(partes[i + 2], out colunas) && int.TryParse(partes[i + 4], out bombas))
                     {
                         break;
@@ -30,127 +34,123 @@ class Program
                 }
             }
 
-            Console.WriteLine(linhas + " Linhas, " + colunas + " colunas e " + bombas + " bombas.");
+
+            Console.WriteLine(+linhas + " Linhas, " + colunas + " colunas e " + bombas + " bombas.");
             Console.WriteLine();
-        
-        
+            Metodos.PrintaMapaCoberto(linhas, colunas);
 
-        Console.WriteLine(+linhas + " Linhas, " + colunas + " colunas e " + bombas + " bombas.");
-        Console.WriteLine();
-        Metodos.printamapaCoberto(linhas, colunas);
+            // WHILE QUE RODA O MAPA ATE A DERROTA OU VITORIA DO JOGADOR 
 
-        // WHILE QUE RODA O MAPA ATE A DERROTA OU VITORIA DO JOGADOR 
+            bool wl = true;
 
-        bool wl = true;
-
-        char[,] mapaX = Metodos.mapacoberto(linhas, colunas);
-        char[,] mapaGabarito = Metodos.Gabarito(linhas, colunas, bombas);
+            char[,] mapaX = Metodos.MapaCoberto(linhas, colunas);
+            char[,] mapaGabarito = Metodos.Gabarito(linhas, colunas, bombas);
 
 
-        while (wl == true)
-        {
-            // RECEBE AS COORDENADAS
-            Console.WriteLine();
-            Console.WriteLine("Digite a linha: ");
-            int linhap = int.Parse(Console.ReadLine()) - 1;
-            Console.WriteLine("Digite a coluna: ");
-            int colunap = int.Parse(Console.ReadLine()) - 1;
-            Console.WriteLine();
-
-
-            //RECEBE OS METODOS DE VITORIA, DERROTA E DE ATUALIZAÇÃO DO MAPA
-            bool verificaDerrota = Metodos.vrfderrota(mapaGabarito, linhap, colunap);
-            char[,] campo = Metodos.RevelaMapa(mapaX, mapaGabarito, linhap, colunap, linhas, colunas);
-            bool verificaWin = Metodos.VerfWin(campo, mapaGabarito, linhas, colunas);
-            int[] indicaColunas = new int[colunas];
-
-            if (verificaDerrota == true) // VERIFICA SE O USUARIO PERDEU E ENCERRA O WHILE
+            while (wl == true)
             {
-                wl = false;
+                // RECEBE AS COORDENADAS
                 Console.WriteLine();
-                int cont = 1;
-                for (int g = 0; g < linhas; g++)
+                Console.WriteLine("Digite a linha: ");
+                int linhap = int.Parse(Console.ReadLine()) - 1;
+                Console.WriteLine("Digite a coluna: ");
+                int colunap = int.Parse(Console.ReadLine()) - 1;
+                Console.WriteLine();
+
+
+                //RECEBE OS METODOS DE VITORIA, DERROTA E DE ATUALIZAÇÃO DO MAPA
+                bool verificaDerrota = Metodos.VerificaDerrota(mapaGabarito, linhap, colunap);
+                char[,] campo = Metodos.RevelaMapa(mapaX, mapaGabarito, linhap, colunap, linhas, colunas);
+                bool verificaWin = Metodos.VerificafWin(campo, mapaGabarito, linhas, colunas);
+                int[] indicaColunas = new int[colunas];
+
+                if (verificaDerrota == true) // VERIFICA SE O USUARIO PERDEU E ENCERRA O WHILE
                 {
-                    if (cont > 9)
+                    wl = false;
+                    Console.WriteLine();
+                    int cont = 1;
+                    for (int g = 0; g < linhas; g++)
                     {
-                        Console.Write(cont++ + "- ");
+                        if (cont > 9)
+                        {
+                            Console.Write(cont++ + "- ");
+                        }
+                        else
+                            Console.Write(cont++ + " - ");
+                        for (int a = 0; a < colunas; a++)
+                        {
+                            Console.Write(campo[g, a] + " ");
+                        }
+                        Console.WriteLine();
                     }
-                    else
-                        Console.Write(cont++ + " - ");
-                    for (int a = 0; a < colunas; a++)
+                    Console.Write("\n" + "    ");
+                    for (int i = 0; i < colunas; i++)
                     {
-                        Console.Write(campo[g, a] + " ");
+                        indicaColunas[i] = i + 1;
+                        Console.Write(indicaColunas[i] + " ");
+                    }
+                    Console.WriteLine("\n");
+                    Console.WriteLine();
+                    Console.Write("VOCÊ PERDEU!");
+
+                }
+                else if (verificaWin == true) // VERIFICA SE O USUARIO GANHOU E ENCERRA O WHILE
+                {
+                    int cont = 1;
+                    for (int g = 0; g < linhas; g++)
+                    {
+                        if (cont > 9)
+                        {
+                            Console.Write(cont++ + "- ");
+                        }
+                        else
+                            Console.Write(cont++ + " - ");
+                        for (int a = 0; a < colunas; a++)
+                        {
+                            Console.Write(campo[g, a] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Write("\n" + "    ");
+                    for (int i = 0; i < colunas; i++)
+                    {
+                        indicaColunas[i] = i + 1;
+                        Console.Write(indicaColunas[i] + " ");
+                    }
+                    Console.WriteLine();
+                    Console.Write("VOCÊ GANHOU!");
+                    break;
+                }
+                else // PRINTA O MAPA DENTRO DO WHILE ATUALIZANDO DE ACORDO COM AS COODERNADAS DIGITAS 
+                {
+
+                    int cont = 1;
+                    for (int g = 0; g < linhas; g++)
+                    {
+                        if (cont > 9)
+                        {
+                            Console.Write(cont++ + "- ");
+                        }
+                        else
+                            Console.Write(cont++ + " - ");
+                        for (int a = 0; a < colunas; a++)
+                        {
+                            Console.Write(campo[g, a] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.Write("\n" + "    ");
+                    for (int i = 0; i < colunas; i++)
+                    {
+                        indicaColunas[i] = i + 1;
+                        Console.Write(indicaColunas[i] + " ");
                     }
                     Console.WriteLine();
                 }
-                Console.Write("\n" + "    ");
-                for (int i = 0; i < colunas; i++)
-                {
-                    indicaColunas[i] = i + 1;
-                    Console.Write(indicaColunas[i] + " ");
-                }
-                Console.WriteLine("\n");
-                Console.WriteLine();
-                Console.Write("VOCÊ PERDEU!");
 
-            }
-            else if (verificaWin == true) // VERIFICA SE O USUARIO GANHOU E ENCERRA O WHILE
-            {
-                int cont = 1;
-                for (int g = 0; g < linhas; g++)
-                {
-                    if (cont > 9)
-                    {
-                        Console.Write(cont++ + "- ");
-                    }
-                    else
-                        Console.Write(cont++ + " - ");
-                    for (int a = 0; a < colunas; a++)
-                    {
-                        Console.Write(campo[g, a] + " ");
-                    }
-                    Console.WriteLine();
-                }
-                Console.Write("\n" + "    ");
-                for (int i = 0; i < colunas; i++)
-                {
-                    indicaColunas[i] = i + 1;
-                    Console.Write(indicaColunas[i] + " ");
-                }
-                Console.WriteLine();
-                Console.Write("VOCÊ GANHOU!");
-                break;
-            }
-            else // PRINTA O MAPA DENTRO DO WHILE ATUALIZANDO DE ACORDO COM AS COODERNADAS DIGITAS 
-            {
-
-                int cont = 1;
-                for (int g = 0; g < linhas; g++)
-                {
-                    if (cont > 9)
-                    {
-                        Console.Write(cont++ + "- ");
-                    }
-                    else
-                        Console.Write(cont++ + " - ");
-                    for (int a = 0; a < colunas; a++)
-                    {
-                        Console.Write(campo[g, a] + " ");
-                    }
-                    Console.WriteLine();
-                }
-                Console.Write("\n" + "    ");
-                for (int i = 0; i < colunas; i++)
-                {
-                    indicaColunas[i] = i + 1;
-                    Console.Write(indicaColunas[i] + " ");
-                }
-                Console.WriteLine();
             }
 
         }
-
-    }
 
         catch (FileNotFoundException) // VERIFICA SE O CAMINHO INFORMADO EXISTE 
         {
@@ -158,4 +158,4 @@ class Program
         }
 
     }
-    }
+}
